4f9a3e49b6cca881bed20ee0c814f774ade068ef
Logan Payton <Logan.Payton@javs.com>
2017-12-06 14:22:05 -0500

    Case for buttons in script

--- www/scripts/action.sh
+++ www/scripts/action.sh
@@ -6,4 +6,48 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 # Action argument should be available in $1
-echo $1
+action="$1"
+
+case ${action} in
+	"channelUp") echo ""
+	;;
+	"channelDown") echo ""
+	;;
+	"volumeUp") echo ""
+	;;
+	"volumeUp") echo ""
+	;;
+	"power") echo ""
+	;;
+	"up") echo ""
+	;;
+	"down") echo ""
+	;;
+	"left") echo ""
+	;;
+	"right") echo ""
+	;;
+	"select") echo ""
+	;;
+	"channel0") echo ""
+	;;
+	"channel1") echo ""
+	;;
+	"channel2") echo ""
+	;;
+	"channel3") echo ""
+	;;
+	"channel4") echo ""
+	;;
+	"channel5") echo ""
+	;;
+	"channel6") echo ""
+	;;
+	"channel7") echo ""
+	;;
+	"channel8") echo ""
+	;;
+	"channel9") echo ""
+	;;
+esac
+
