5be52fd7126f53e69a66cc75c531f776d7f36c29
Logan Payton <18620874+EnsignPayton@users.noreply.github.com>
2017-12-09 11:53:49 -0500

    Fix path

--- www/html/index.php
+++ www/html/index.php
@@ -7,7 +7,7 @@
 
 		<!-- Style Includes -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link rel="stylesheet" href="project.css">
+		<link rel="stylesheet" href="css/project.css">
 	</head>
 	<body>
 		<div class="container">
@@ -100,7 +100,7 @@
 		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
-		<script src="project.js"></script>
+		<script src="js/project.js"></script>
 	</body>
 </html>
 
